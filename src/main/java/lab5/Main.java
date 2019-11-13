6a71ea0bb2d946ba09fab7073b0ddc89ce2743c5
Ivan <Ivan Filonenko>
2019-11-13 14:34:25 +0300

    Scripted auto-commit on change (2019-11-13 14:34:25) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -1,4 +1,13 @@
 package lab5;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+
 public class Main {
+    System.out.println("start!");
+    ActorSystem system = ActorSystem.create("routes");
+    final Http http = Http.get(system);
+    final ActorMaterializer materializer =
+            ActorMaterializer.create(system);
 }
