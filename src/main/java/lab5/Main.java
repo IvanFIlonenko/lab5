bb00fc6e8d026bd45fd55600c600e0dd47399637
Ivan <Ivan Filonenko>
2019-11-13 14:34:44 +0300

    Scripted auto-commit on change (2019-11-13 14:34:44) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -4,10 +4,14 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
+import java.io.IOException;
+
 public class Main {
-    System.out.println("start!");
-    ActorSystem system = ActorSystem.create("routes");
-    final Http http = Http.get(system);
-    final ActorMaterializer materializer =
-            ActorMaterializer.create(system);
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+    }
 }
