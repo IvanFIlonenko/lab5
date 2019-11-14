edd6c5bd6ed78e85d2cca74497bd3be690fad1ca
Ivan <Ivan Filonenko>
2019-11-14 19:35:37 +0300

    Scripted auto-commit on change (2019-11-14 19:35:37) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -64,17 +64,19 @@ public class Main {
                                                     return Source.from(Collections.singleton(pair)).toMat(Flow.<Pair<HttpRequest, Integer>>create().
                                                             mapConcat(p -> Collections.nCopies(p.second(), p.first())).
                                                             mapAsync(1, request2 ->{
-                                                                CompletableFuture<Long> future = CompletableFuture.supplyAsync(() -> {
-                                                                    long start = System.nanoTime();
+                                                                CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> {
                                                                     ListenableFuture<Response> whenResponse = asyncHttpClient().prepareGet(request2.toString()).execute();
                                                                     try {
                                                                         Response response = whenResponse.get();
                                                                     } catch (InterruptedException | ExecutionException e) {
                                                                     }
+                                                                    return true;
+                                                                });
+                                                                CompletableFuture<Long> f = future.thenCompose(job -> {
+                                                                    long start = System.nanoTime();
                                                                     long elapsedTime = System.nanoTime() - start;
                                                                     return elapsedTime;
-                                                                });
-                                                                CompletableFuture<Long> f = future.thenAccept()
+                                                                })
                                                             })
                                                             .toMat(
                                                                     fold, Keep.right()), Keep.right()).run(materializer);
