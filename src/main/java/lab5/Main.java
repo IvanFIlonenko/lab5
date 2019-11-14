eb4a5c67ae404e342045d7ac26586b656987103a
Ivan <Ivan Filonenko>
2019-11-14 16:56:54 +0300

    Scripted auto-commit on change (2019-11-14 16:56:54) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -43,6 +43,7 @@ public class Main {
                             Flow<Pair<String,Integer>,HttpResponse, NotUsed> flow = Flow.<Pair<String, Integer>>create().
                                     mapAsync(1, p ->{
                                         Patterns.ask(controlActor, new PairMsg(p), 5000);
+                                        return "kek";
                                     })
                             return HttpResponse.create().withEntity(ContentTypes.TEXT_HTML_UTF8,
                                     ByteString.fromString("kek"));
--- src/main/java/lab5/PairMsg.java
+++ src/main/java/lab5/PairMsg.java
@@ -1,4 +1,13 @@
 package lab5;
 
+import com.sun.tools.javac.util.Pair;
+import scala.Int;
+import sun.rmi.server.InactiveGroupException;
+
 public class PairMsg {
+    Pair<String, Integer> msgPair;
+
+    public PairMsg(Pair<String, Integer> pair) {
+        this.msgPair = pair;
+    }
 }
