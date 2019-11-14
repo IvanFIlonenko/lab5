923dae5d4c01e183a2639f3be313a48ece716e0d
Ivan <Ivan Filonenko>
2019-11-14 20:08:18 +0300

    Scripted auto-commit on change (2019-11-14 20:08:18) by gitwatch.sh

--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -84,7 +84,7 @@ public class Main {
                                 CompletionStage<HttpResponse> result = source.via(flow).toMat(Sink.last(), Keep.right()).run(materializer);
                                 return result.toCompletableFuture().get();
                             } catch (Exception e) {
-                                return HttpResponse.create().withEntity(ByteString.fromString("count is not an integer"));
+                                return HttpResponse.create().withEntity(ByteString.fromString(e.toString()));
                             }
                         } else {
                             request.discardEntityBytes(materializer);
