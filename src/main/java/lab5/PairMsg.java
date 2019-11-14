daf70ed05c461e3ac48e82357cde45e1cfe4d022
Ivan <Ivan Filonenko>
2019-11-14 18:48:01 +0300

    Scripted auto-commit on change (2019-11-14 18:48:01) by gitwatch.sh

--- src/main/java/lab5/PairMsg.java
+++ src/main/java/lab5/PairMsg.java
@@ -1,9 +1,6 @@
 package lab5;
 
 import com.sun.tools.javac.util.Pair;
-import scala.Int;
-import sun.rmi.server.InactiveGroupException;
-
 public class PairMsg {
     Pair<String, Integer> msgPair;
 
