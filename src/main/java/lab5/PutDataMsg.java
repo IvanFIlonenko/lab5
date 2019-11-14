1c07244da4ad35723331007c9e24361cb87cca20
Ivan <Ivan Filonenko>
2019-11-14 21:52:46 +0300

    Scripted auto-commit on change (2019-11-14 21:52:46) by gitwatch.sh

--- src/main/java/lab5/PutDataMsg.java
+++ src/main/java/lab5/PutDataMsg.java
@@ -1,4 +1,18 @@
 package lab5;
 
+import javafx.util.Pair;
+
 public class PutDataMsg {
+    private Pair<String, Integer> msg;
+    public GetDataMsg(Pair<String, Integer> msg){
+        this.msg = msg;
+    }
+
+    public String getUrl(){
+        return msg.getKey();
+    }
+
+    public Integer getValue(){
+        return msg.getValue();
+    }
 }
